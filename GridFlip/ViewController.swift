64cffe2620b55ee5d363eff2b3bea66536f1757b
Michael Vilabrera <m.vilabrera@gmail.com>
2018-03-19 13:15:42 -0400

    initial commit

---
+++ GridFlip/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  GridFlip
+//
+//  Created by Michael Vilabrera on 3/19/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+    
+    @IBAction func randomizeButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath)
+        return cell
+    }
+}
